5105e48cd535611fe0a9eac262a9a1dcdbc70edb
myxy99 <2399158611@qq.com>
2020-12-23 23:51:20 +0800

    xflag

--- pkg/xflag/xflag.go
+++ pkg/xflag/xflag.go
@@ -1,337 +1,261 @@
-package xflag
+/**
+* @Author: myxy99 <myxy99@foxmail.com>
+* @Date: 2020/12/23 23:24
+ */
+package xfalg
 
 import (
-	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"github.com/spf13/cobra"
+	"sync"
+	"time"
+)
+
+type (
+	Command         = cobra.Command
+	FlagRegisterFun func(*Command)
+	CommandNode     struct {
+		Name    string
+		Command *Command
+		Flags   FlagRegisterFun
+	}
+	storage struct {
+		instances sync.Map
+	}
 )
 
 var (
-	flagset *FlagSet
-)
-
-var defaultFlags = []Flag{
-	// HelpFlag prints usage of application.
-	&BoolFlag{
-		Name:  "help",
-		Usage: "--help, show help information",
-		Action: func(name string, fs *FlagSet) {
-			fs.PrintDefaults()
-			os.Exit(0)
-		},
-	},
-}
-
-func init() {
-	// procName := filepath.Base(os.Args[0])
-	// nfs := flag.NewFlagSet(procName, flag.ExitOnError)
-	flagset = &FlagSet{
-		FlagSet:  flag.CommandLine,
-		flags:    defaultFlags,
-		actions:  make(map[string]func(string, *FlagSet)),
-		environs: make(map[string]string),
+	defaultFlags = &Command{
+		Use:                "app",
+		DisableSuggestions: false,
 	}
-}
-
-// Flag ...
-type (
-	// Flag defines application flag.
-	Flag interface {
-		Apply(*FlagSet)
-	}
-
-	// FlagSet wraps a set of Flags.
-	FlagSet struct {
-		*flag.FlagSet
-		flags    []Flag
-		actions  map[string]func(string, *FlagSet)
-		environs map[string]string
+	bucket = &storage{
+		instances: sync.Map{},
 	}
 )
 
-// Register ...
-func Register(fs ...Flag) {
-	flagset.Register(fs...)
+func NewRootCommand(c *Command) {
+	defaultFlags = c
 }
 
-// Register ...
-func (fs *FlagSet) Register(flags ...Flag) {
-	fs.flags = append(fs.flags, flags...)
+func Register(fs ...CommandNode) {
+	for _, c := range fs {
+		c.Flags(c.Command)
+		defaultFlags.AddCommand(c.Command)
+		bucket.instances.Store(c.Name, c.Command)
+	}
 }
 
-// With adds flags to the flagset.
-func With(fs ...Flag) { flagset.With(fs...) }
-
-// With adds flags to provided flagset.
-func (fs *FlagSet) With(flags ...Flag) {
-	fs.flags = append(fs.flags, flags...)
-}
-
-// Parse parses the flagset.
 func Parse() error {
-	return flagset.Parse()
+	return defaultFlags.Execute()
 }
 
-// Lookup lookup flag value by name
-// priority: flag > default > env
-func (fs *FlagSet) Lookup(name string) *flag.Flag {
-	flag := fs.FlagSet.Lookup(name)
-	if flag != nil {
-		if flag.Value.String() == "" {
-			if env, ok := fs.environs[name]; ok {
-				flag.Value.Set(env)
-			}
-		}
-		if flag.Value.String() == "" {
-			flag.Value.Set(flag.DefValue)
-		}
-	}
-	return flag
+func NString(nodeName, name string) string {
+	v, _ := NStringE(nodeName, name)
+	return v
 }
 
-// Parse parses provided flagset.
-func (fs *FlagSet) Parse() error {
-	if fs.Parsed() {
-		return nil
-	}
-	for _, f := range fs.flags {
-		f.Apply(fs)
-	}
-
-	if err := fs.FlagSet.Parse(os.Args[1:]); err != nil {
-		return err
-	}
-
-	fs.FlagSet.Visit(func(f *flag.Flag) {
-		if action, ok := fs.actions[f.Name]; ok && action != nil {
-			action(f.Name, fs)
-		}
-		if env, ok := fs.environs[f.Name]; ok {
-			fs.environs[f.Name] = env
-		}
-	})
-
-	return nil
+func NBool(nodeName, name string) bool {
+	v, _ := NBoolE(nodeName, name)
+	return v
 }
 
-// BoolE parses bool flag of the flagset with error returned.
-func BoolE(name string) (bool, error) { return flagset.BoolE(name) }
-
-// BoolE parses bool flag of provided flagset with error returned.
-func (fs *FlagSet) BoolE(name string) (bool, error) {
-	flag := fs.Lookup(name)
-	if flag != nil {
-		return strconv.ParseBool(flag.Value.String())
-	}
-
-	return false, fmt.Errorf("undefined flag name: %s", name)
+func NTimeDuration(nodeName, name string) time.Duration {
+	v, _ := NTimeDurationE(nodeName, name)
+	return v
 }
 
-// Bool parses bool flag of the flagset.
-func Bool(name string) bool { return flagset.Bool(name) }
-
-// Bool parses bool flag of provided flagset.
-func (fs *FlagSet) Bool(name string) bool {
-	ret, _ := fs.BoolE(name)
-	return ret
+func NFloat32(nodeName, name string) float32 {
+	v, _ := NFloat32E(nodeName, name)
+	return v
 }
 
-// StringE parses string flag of the flagset with error returned.
-func StringE(name string) (string, error) { return flagset.StringE(name) }
+func NFloat64(nodeName, name string) float64 {
+	v, _ := NFloat64E(nodeName, name)
+	return v
+}
 
-// StringE parses string flag of provided flagset with error returned.
-func (fs *FlagSet) StringE(name string) (string, error) {
-	flag := fs.Lookup(name)
-	if flag != nil {
-		return flag.Value.String(), nil
+func NInt(nodeName, name string) int {
+	v, _ := NIntE(nodeName, name)
+	return v
+}
+
+func NStringSlice(nodeName, name string) []string {
+	v, _ := NStringSliceE(nodeName, name)
+	return v
+}
+
+func String(name string) string {
+	v, _ := StringE(name)
+	return v
+}
+
+func Bool(name string) bool {
+	v, _ := BoolE(name)
+	return v
+}
+
+func TimeDuration(name string) time.Duration {
+	v, _ := TimeDurationE(name)
+	return v
+}
+
+func Float32(name string) float32 {
+	v, _ := Float32E(name)
+	return v
+}
+
+func Float64(name string) float64 {
+	v, _ := Float64E(name)
+	return v
+}
+
+func Int(name string) int {
+	v, _ := IntE(name)
+	return v
+}
+
+func StringSlice(name string) []string {
+	v, _ := StringSliceE(name)
+	return v
+}
+
+func NStringE(nodeName, name string) (string, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetString(name)
 	}
-
 	return "", fmt.Errorf("undefined flag name: %s", name)
 }
 
-// String parses string flag of the flagset.
-func String(name string) string { return flagset.String(name) }
-
-// String parses string flag of provided flagset.
-func (fs *FlagSet) String(name string) string {
-	ret, _ := fs.StringE(name)
-	return ret
+func NBoolE(nodeName, name string) (bool, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetBool(name)
+	}
+	return false, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// IntE parses int flag of the flagset with error returned.
-func IntE(name string) (int64, error) { return flagset.IntE(name) }
-
-// IntE parses int flag of provided flagset with error returned.
-func (fs *FlagSet) IntE(name string) (int64, error) {
-	flag := fs.Lookup(name)
-	if flag != nil {
-		return strconv.ParseInt(flag.Value.String(), 10, 64)
+func NTimeDurationE(nodeName, name string) (time.Duration, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetDuration(name)
 	}
-
 	return 0, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// Int parses int flag of the flagset.
-func Int(name string) int64 { return flagset.Int(name) }
-
-// Int parses int flag of provided flagset.
-func (fs *FlagSet) Int(name string) int64 {
-	ret, _ := fs.IntE(name)
-	return ret
-}
-
-// UintE parses int flag of the flagset with error returned.
-func UintE(name string) (uint64, error) { return flagset.UintE(name) }
-
-// UintE parses int flag of provided flagset with error returned.
-func (fs *FlagSet) UintE(name string) (uint64, error) {
-	flag := fs.Lookup(name)
-	if flag != nil {
-		return strconv.ParseUint(flag.Value.String(), 10, 64)
+func NFloat32E(nodeName, name string) (float32, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetFloat32(name)
 	}
-
 	return 0, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// Uint parses int flag of the flagset.
-func Uint(name string) uint64 { return flagset.Uint(name) }
-
-// Uint parses int flag of provided flagset.
-func (fs *FlagSet) Uint(name string) uint64 {
-	ret, _ := fs.UintE(name)
-	return ret
-}
-
-// Float64E parses int flag of the flagset with error returned.
-func Float64E(name string) (float64, error) { return flagset.Float64E(name) }
-
-// Float64E parses int flag of provided flagset with error returned.
-func (fs *FlagSet) Float64E(name string) (float64, error) {
-	flag := fs.Lookup(name)
-	if flag != nil {
-		return strconv.ParseFloat(flag.Value.String(), 64)
+func NFloat64E(nodeName, name string) (float64, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetFloat64(name)
 	}
-
-	return 0.0, fmt.Errorf("undefined flag name: %s", name)
+	return 0, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// Float64 parses int flag of the flagset.
-func Float64(name string) float64 { return flagset.Float64(name) }
-
-// Float64 parses int flag of provided flagset.
-func (fs *FlagSet) Float64(name string) float64 {
-	ret, _ := fs.Float64E(name)
-	return ret
-}
-
-// BoolFlag is a bool flag implements of Flag interface.
-type BoolFlag struct {
-	Name     string
-	Usage    string
-	EnvVar   string
-	Default  bool
-	Variable *bool
-	Action   func(string, *FlagSet)
-}
-
-// Apply implements of Flag Apply function.
-func (f *BoolFlag) Apply(set *FlagSet) {
-	for _, field := range strings.Split(f.Name, ",") {
-		field = strings.TrimSpace(field)
-		if f.Variable != nil {
-			set.FlagSet.BoolVar(f.Variable, field, f.Default, f.Usage)
-		}
-
-		set.FlagSet.Bool(field, f.Default, f.Usage)
-		set.actions[field] = f.Action
-		set.environs[field] = os.Getenv(f.EnvVar)
+func NIntE(nodeName, name string) (int, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetInt(name)
 	}
+	return 0, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// StringFlag is a string flag implements of Flag interface.
-type StringFlag struct {
-	Name     string
-	Usage    string
-	EnvVar   string
-	Default  string
-	Variable *string
-	Action   func(string, *FlagSet)
-}
-
-// Apply implements of Flag Apply function.
-func (f *StringFlag) Apply(set *FlagSet) {
-	for _, field := range strings.Split(f.Name, ",") {
-		field = strings.TrimSpace(field)
-		if f.Variable != nil {
-			set.FlagSet.StringVar(f.Variable, field, f.Default, f.Usage)
-		}
-		set.FlagSet.String(field, f.Default, f.Usage)
-		set.actions[field] = f.Action
-		set.environs[field] = os.Getenv(f.EnvVar)
+func NStringSliceE(nodeName, name string) ([]string, error) {
+	if val, ok := bucket.instances.Load(nodeName); ok {
+		return val.(*Command).Flags().GetStringSlice(name)
 	}
+	return nil, fmt.Errorf("undefined flag name: %s", name)
 }
 
-// IntFlag is an int flag implements of Flag interface.
-type IntFlag struct {
-	Name     string
-	Usage    string
-	Default  int
-	Variable *int
-	Action   func(string, *FlagSet)
-}
-
-// Apply implements of Flag Apply function.
-func (f *IntFlag) Apply(set *FlagSet) {
-	for _, field := range strings.Split(f.Name, ",") {
-		field = strings.TrimSpace(field)
-		if f.Variable != nil {
-			set.FlagSet.IntVar(f.Variable, field, f.Default, f.Usage)
-		}
-		set.FlagSet.Int(field, f.Default, f.Usage)
-		set.actions[field] = f.Action
+func StringE(name string) (val string, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetString(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
 	}
+	return
 }
 
-// UintFlag is an uint flag implements of Flag interface.
-type UintFlag struct {
-	Name     string
-	Usage    string
-	Default  uint
-	Variable *uint
-	Action   func(string, *FlagSet)
-}
-
-// Apply implements of Flag Apply function.
-func (f *UintFlag) Apply(set *FlagSet) {
-	for _, field := range strings.Split(f.Name, ",") {
-		field = strings.TrimSpace(field)
-		if f.Variable != nil {
-			set.FlagSet.UintVar(f.Variable, field, f.Default, f.Usage)
-		}
-		set.FlagSet.Uint(field, f.Default, f.Usage)
-		set.actions[field] = f.Action
+func BoolE(name string) (val bool, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetBool(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
 	}
+	return
 }
 
-// Float64Flag is a float flag implements of Flag interface.
-type Float64Flag struct {
-	Name     string
-	Usage    string
-	Default  float64
-	Variable *float64
-	Action   func(string, *FlagSet)
-}
-
-// Apply implements of Flag Apply function.
-func (f *Float64Flag) Apply(set *FlagSet) {
-	for _, field := range strings.Split(f.Name, ",") {
-		field = strings.TrimSpace(field)
-		if f.Variable != nil {
-			set.FlagSet.Float64Var(f.Variable, field, f.Default, f.Usage)
-		}
-		set.FlagSet.Float64(field, f.Default, f.Usage)
-		set.actions[field] = f.Action
+func TimeDurationE(name string) (val time.Duration, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetDuration(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
 	}
+	return
+}
+
+func Float32E(name string) (val float32, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetFloat32(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
+	}
+	return
+}
+
+func Float64E(name string) (val float64, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetFloat64(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
+	}
+	return
+}
+
+func IntE(name string) (val int, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetInt(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
+	}
+	return
+}
+
+func StringSliceE(name string) (val []string, err error) {
+	var ok bool
+	bucket.instances.Range(func(key, value interface{}) bool {
+		val, err = value.(*Command).Flags().GetStringSlice(name)
+		ok = err == nil
+		return err != nil
+	})
+	if !ok {
+		err = fmt.Errorf("undefined flag name: %s", name)
+	}
+	return
 }
