d178e1834477631313fc5ba40e102580d187f3d7
myxy99 <2399158611@qq.com>
2021-03-10 22:45:53 +0800

    mod

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-redis/redis/v8 v8.7.1
 	github.com/golang/protobuf v1.4.3
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/philchia/agollo/v4 v4.1.3
 	github.com/pkg/errors v0.9.1
--- xregistry/xetcd/etcd_test.go
+++ xregistry/xetcd/etcd_test.go
@@ -3,33 +3,19 @@ package xetcd
 import (
 	"context"
 	"fmt"
+	"github.com/coder2z/g-saber/xtime"
 	"github.com/coder2z/g-server/xgrpc"
 	xbalancer "github.com/coder2z/g-server/xgrpc/balancer"
 	"github.com/coder2z/g-server/xgrpc/balancer/least_connection"
 	clientinterceptors "github.com/coder2z/g-server/xgrpc/client"
 	serverinterceptors "github.com/coder2z/g-server/xgrpc/server"
 	"github.com/coder2z/g-server/xregistry"
-	"github.com/coder2z/g-saber/xlog"
-	"github.com/coder2z/g-saber/xtime"
-	proto "github.com/grpc-ecosystem/go-grpc-prometheus/examples/grpc-server-with-prometheus/protobuf"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
-	"log"
 	"net"
 	"testing"
 	"time"
 )
-
-type server struct {
-	add string
-}
-
-func (s server) SayHello(c context.Context, request *proto.HelloRequest) (*proto.HelloResponse, error) {
-	text := "Hello " + request.Name + ", I am " + s.add
-	log.Println(text)
-
-	return &proto.HelloResponse{Message: text}, nil
-}
 
 func TestEtcd(t *testing.T) {
 
@@ -65,7 +51,6 @@ func TestEtcd(t *testing.T) {
 	}
 	serve := grpc.NewServer(options...)
 	listener, err := net.Listen("tcp", ":8888")
-	proto.RegisterDemoServiceServer(serve, server{add: ":8888"})
 	_ = serve.Serve(listener)
 }
 
@@ -105,7 +90,6 @@ func TestEtcd2(t *testing.T) {
 	)
 
 	listener, err := net.Listen("tcp", ":7777")
-	proto.RegisterDemoServiceServer(serve, server{add: ":7777"})
 	_ = serve.Serve(listener)
 }
 
@@ -142,19 +126,4 @@ func TestEtcdDiscovery(t *testing.T) {
 		return
 	}
 	defer conn.Close()
-	client := proto.NewDemoServiceClient(conn)
-	for i := 0; i < 100; i++ {
-		ctx := context.Background()
-		resp, err := client.SayHello(ctx,
-			&proto.HelloRequest{
-				Name: "123",
-			})
-		if err != nil {
-			xlog.Error("error", xlog.FieldErr(err))
-			time.Sleep(time.Second)
-			continue
-		}
-		xlog.Info("success", xlog.String("msg", resp.GetMessage()))
-		time.Sleep(time.Second)
-	}
 }
